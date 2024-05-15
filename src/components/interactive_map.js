d1bcbe524cd8e3b4a3858e7c8bfc2f997c66660d
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2024-05-15 17:39:41 +0200

    Add renove_1888_tiles

--- src/components/interactive_map.js
+++ src/components/interactive_map.js
@@ -1,3 +1,4 @@
+import * as L from "npm:leaflet";
 
 export function get_feature_style(feature) {
     const style = {
@@ -23,17 +24,14 @@ export function get_extracted_feature_style(feature) {
     const style = {
         weight: 1,
         opacity: 1.0,
-        fillOpacity: 1.0
+        fillOpacity: 1.0,
+        color: "#000000"
     };
 
-    if (feature.properties.use.includes("moulin")) { style.color = "#ff0000"; }
-    else if (feature.properties.use.includes("tannerie")) { style.color = "#00ff00"; }
-    else if (feature.properties.use.includes("scierie")) { style.color = "#0000ff"; }
-    else { style.color = "#ff00ff"; }
+    if (feature.properties.use.includes("moulin")) { style.fillColor = "#ff0000"; }
+    else if (feature.properties.use.includes("tannerie")) { style.fillColor = "#00ff00"; }
+    else if (feature.properties.use.includes("scierie")) { style.fillColor = "#0000ff"; }
+    else { style.fillColor = "#ff00ff"; }
 
     return style;
-}
-
-export function on_each_feature(feature, layer) {
-    layer.bindPopup("BLABLABLA"); 
 }
